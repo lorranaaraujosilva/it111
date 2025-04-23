22610f553237c6cdfd6ba9a677561a34df92daeb
lorranaaraujosilva <lorrana-las@hotmail.com>
2025-04-23 08:00:14 -0700

    Add files via upload

---
+++ MyFirstProgram.java
@@ -0,0 +1,9 @@
+
+// hello world example
+public class MyFirstProgram {
+
+    public static void main (String[] args) {
+        System.out.println("Hello World!!");
+}  // end main
+
+} // end
---
+++ MyVariableOne.java
@@ -0,0 +1,31 @@
+public class MyVariableOne {
+
+public static void main (String[] args){
+    String name = "Lorrana"; // after a statement, don't forget the semi colon!
+    int hours = 40; // we are assigning 40 to the hour variables
+    // please note, when we have a string value, it is nested in ""
+    // when we are refering to numbers/slash integers, no ""
+    System.out.println(name);
+    System.out.println(hours);
+
+    String building, tree, lake, ocean;
+    tree = "Redwood";
+    building = "empire state";
+    lake = "lake superior";
+    ocean = "pacific";
+
+    int weeks, months, decades, years;
+    weeks = 4;
+    decades = 10;
+    years = 30;
+    months = 12;
+    System.out.println(tree);
+    System.out.println(building);
+    System.out.println(lake);
+    System.out.println(ocean);
+    System.out.println(weeks);
+    System.out.println(months);
+    System.out.println(decades);
+    System.out.println(years);
+    }
+}
